ac1f02b724bbdfc2aa286ac4d14b18faf1c7f041
Miguel <miguelprudencio93@gmail.com>
2020-12-01 21:47:02 -0300

    criado o atributo nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,19 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.idade = idade
+        self.nome = nome
+
     def comprimentar(self):
         return f'ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Miguel', 26)
     print(Pessoa.comprimentar(p))
-    print(p)
+    print(id(p))
     print(p.comprimentar())
+    print(p.nome)
+    print(p.idade)
+    p.nome = 'francisco'
+    p.idade = 40
+    print(p.nome)
+    print(p.idade)
