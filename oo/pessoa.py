23375a8456e9a7e95258045290d33df706a28ce7
Miguel <miguelprudencio93@gmail.com>
2020-12-05 18:16:27 -0300

    Criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filho,nome=None, idade=None):
         self.idade = idade
         self.nome = nome
+        self.filho = list(filho)
 
     def comprimentar(self):
         return f'ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Miguel', 26)
-    print(Pessoa.comprimentar(p))
-    print(id(p))
-    print(p.comprimentar())
-    print(p.nome)
-    print(p.idade)
-    p.nome = 'francisco'
-    p.idade = 40
-    print(p.nome)
-    print(p.idade)
+    kelvem = Pessoa(nome='kelvem', idade=11)
+    miguel = Pessoa(kelvem, nome='Miguel', idade=26)
+    print(Pessoa.comprimentar(miguel))
+    print(id(miguel))
+    print(miguel.comprimentar())
+    print(miguel.nome)
+    print(miguel.idade)
+    for filho in miguel.filho:
+        print(filho.nome)
+
+
